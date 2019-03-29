d3145a5ab5946483dace0f267e83a3a1eb7aba1b
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2019-03-29 19:28:27 +0100

    add some morre stuff

--- toolchain.cmake
+++ toolchain.cmake
@@ -11,20 +11,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-#set(CMAKE_SYSTEM_NAME Linux)
-#set(CMAKE_SYSTEM_PROCESSOR arm)
-#
-#SET(CMAKE_SYSTEM_VERSION 1)
-#
-#set(CMAKE_C_COMPILER arm-linux-gnu-gcc)
-#set(CMAKE_CXX_COMPILER arm-linux-gnu-g++)
-#
-## where is the target environment
-#SET(CMAKE_FIND_ROOT_PATH  /opt/eldk-2007-01-19/ppc_74xx /home/alex/eldk-ppc74xx-inst)
-#
-## search for programs in the build host directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#
-## for libraries and headers in the target directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
